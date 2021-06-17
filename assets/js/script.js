918d67b9c38d0a9cdbd01611f1776e6d576a45bc
RCass172 <ruthcassidy1221@gmail.com>
2021-06-17 20:26:58 +0000

    Update the slideshow function to include testimonial section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,23 @@
 // image slideshow example taken from w3schools.com - https://www.w3schools.com/howto/howto_js_slideshow.asp
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = [1,1];
+let slideId = ["firstSlide", "secondSlide"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides(n, no) {
   let i;
-  let slides = document.getElementsByClassName("slides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-
-  slides[slideIndex-1].style.display = "block";
+  x[slideIndex[no]-1].style.display = "block";
 }
 
 
