e81d95bb8c52c5f873ddd0207d40248018f96f5e
RCass172 <ruthcassidy1221@gmail.com>
2021-06-20 19:09:06 +0000

    Add a marker to the map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,21 @@
 //function called by script on index.html to activate map
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 53.887085, lng: -7.150008},
-        zoom: 15
+
+    // sets coordinates of business home
+    let home = {
+        lat: 53.887085, 
+        lng: -7.150008
+    };
+
+    //creates the map in relevant div centered and zoomed in to location
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: home,
+        zoom: 15,
+    });
+
+    //creates a marker on the map at the specified location coordinates
+    let marker = new google.maps.Marker( {
+        position: home,
+        map: map,
     });
 }
