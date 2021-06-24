ae1af10134b276e8ae51795805e3738d771f266b
RCass172 <ruthcassidy1221@gmail.com>
2021-06-24 14:45:25 +0000

    Add jshint esversion 6 to fix warning

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,19 +1,22 @@
-//function called by script on index.html to activate map
+/*jshint esversion: 6 */ 
+
+//Function called by script on index.html to activate map
 function initMap() {
 
-    // sets coordinates of business home
+    //Sets coordinates of business home
     let home = {
         lat: 53.887085, 
         lng: -7.150008
     };
 
-    //creates the map in relevant div centered and zoomed in to location
+    //Creates the map in relevant div centered and zoomed in to location
     let map = new google.maps.Map(document.getElementById('map'), {
         center: home,
         zoom: 15,
     });
 
-    //creates a marker on the map at the specified location coordinates, Google maps documentation used as reference
+    //Creates a marker on the map at the specified location coordinates
+    //Google maps documentation used as reference
     let marker = new google.maps.Marker( {
         position: home,
         map: map,
